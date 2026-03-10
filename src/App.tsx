40ac42368f1b88a85b1614b90ad1fe2cb13a4413
NicoVegaPortaluppi <nicovporta.luppi@gmail.com>
2026-03-10 16:15:14 +0800

    Testing using buttons as section changers

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,40 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center h-screen bg-black text-white">
-        <h1 className="text-5xl font-bold">Aegis</h1>
-        <p className="mt-4 text-gray-400">Hello</p>
-        <button className="mt-6 bg-blue-500 px-6 py-3 rounded-lg">lol</button>
-      </section>
+      {/*
+        1. h-screen -> Affects all the screen
+        2. flex items-center -> Puts the text in the center of the screen (Large)
+        3. justify-center -> Puts the text in the center of the screen (Width)
+        4. flex flex-col items-center -> Puts the items in the center and one below the other
+        */}
+
+      <div className="flex flex-col items-center h-screen bg-gray-900 gap-y-20">
+        <h1 className=" text-6xl font-bold text-blue-400">Aegis Security</h1>
+        <div className="flex items-center justify-center gap-x-100">
+          <Sections>Section 1</Sections>
+          <Sections>Section 2</Sections>
+          <Sections>Section 3</Sections>
+        </div>
+      </div>
     </>
   );
 }
 
-export default App;
+function Sections(hold) {
+  const [bg, setBgColor] = useState("bg-gray-900");
+  function changeName() {
+    setBgColor("bg-red-100");
+  }
+
+  return (
+    <button
+      className={`text-4xl text-blue-400 font-bold ${bg}`}
+      onClick={changeName}
+    >
+      {hold.children}
+    </button>
+  );
+}
